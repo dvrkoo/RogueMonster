20c88be084528ad146fea618b2044431620c2ac2
dvrkoo <Nico.marini@hotmail.it>
2022-07-31 18:51:26 +0200

    added viewport for better resize on game menu

--- core/src/com/mygdx/game/States/GameState.java
+++ core/src/com/mygdx/game/States/GameState.java
@@ -1,10 +1,11 @@
 package com.mygdx.game.States;
 
-
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.RogueMonster;
 import com.mygdx.game.Characters.Character;
 
@@ -13,71 +14,68 @@ public class GameState implements Screen {
     final RogueMonster game;
     Character player;
 
+    Viewport viewport;
     OrthographicCamera camera;
 
-    //game methods
-    public GameState(final RogueMonster game){
+    // game methods
+    public GameState(final RogueMonster game) {
         this.game = game;
         player = new Character(500, 500);
 
         camera = new OrthographicCamera();
-        camera.setToOrtho(false, 1000,1000);
-
-
+        camera.setToOrtho(false, 1000, 1000);
+        viewport = new FitViewport(1000, 1000, camera);
 
     }
 
     @Override
     public void show() {
         // TODO Auto-generated method stub
-        
+
     }
 
-  
     @Override
     public void render(float delta) {
-       ScreenUtils.clear(0.2f,0,0.2f,1);
+        ScreenUtils.clear(0.2f, 0, 0.2f, 1);
 
-       camera.update();
+        camera.update();
 
-       game.batch.setProjectionMatrix(camera.combined);
+        game.batch.setProjectionMatrix(camera.combined);
 
-       game.batch.begin();
-       game.batch.draw(player.getTexture(), player.getBoundingRectangle().x,player.getBoundingRectangle().y,player.getBoundingRectangle().width,player.getBoundingRectangle().height);
-       game.batch.end();
+        game.batch.begin();
+        game.batch.draw(player.getTexture(), player.getBoundingRectangle().x, player.getBoundingRectangle().y,
+                player.getBoundingRectangle().width, player.getBoundingRectangle().height);
+        game.batch.end();
 
-        
     }
 
     @Override
     public void resize(int width, int height) {
-        // TODO Auto-generated method stub
-        
+        viewport.update(width, height);
+
     }
 
     @Override
     public void pause() {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void resume() {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void hide() {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void dispose() {
-        
 
-        
     }
-    
+
 }
