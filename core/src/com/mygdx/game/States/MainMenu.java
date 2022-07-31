07363e1a191992eb96a5b78028294f39023e2c34
dvrkoo <Nico.marini@hotmail.it>
2022-07-31 17:14:38 +0200

    modified:   core/bin/main/com/mygdx/game/States/MainMenu.class 	modified:   core/src/com/mygdx/game/States/MainMenu.java

--- core/src/com/mygdx/game/States/MainMenu.java
+++ core/src/com/mygdx/game/States/MainMenu.java
@@ -5,22 +5,27 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.RogueMonster;
 
-public class MainMenu implements Screen{
+public class MainMenu implements Screen {
     final RogueMonster game;
     OrthographicCamera camera;
+    private Viewport viewport;
 
-    public MainMenu(final RogueMonster game){
+    public MainMenu(final RogueMonster game) {
         this.game = game;
 
         camera = new OrthographicCamera();
-        camera.setToOrtho(false,1000,1000);
+        camera.setToOrtho(false, 1000, 1000);
+        viewport = new ScreenViewport(camera);
     }
+
     @Override
     public void show() {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
@@ -29,44 +34,42 @@ public class MainMenu implements Screen{
         game.batch.setProjectionMatrix(camera.combined);
 
         game.batch.begin();
-        game.font.draw(game.batch,"MAIN MENU", 500, 500);
+        game.font.draw(game.batch, "MAIN MENU", 500, 500);
         game.batch.end();
-        
-        //change to Game state logic
-        if(Gdx.input.isTouched()){
+
+        // change to Game state logic
+        if (Gdx.input.isTouched()) {
             game.setScreen(new GameState(game));
         }
-        
+
     }
 
     @Override
     public void resize(int width, int height) {
-        //TODO add viewport to handle the resizing problems
-
-        
+        viewport.update(width, height);
     }
 
     @Override
     public void pause() {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void resume() {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void hide() {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void dispose() {
-        
+
     }
-    
+
 }
