332578610ac18b7a08e6cfe8c38da07e7559bfa4
dvrkoo <Nico.marini@hotmail.it>
2022-08-09 19:07:30 +0200

    Added map logic, island works with chunk of tiles

--- core/src/com/mygdx/game/States/GameState.java
+++ core/src/com/mygdx/game/States/GameState.java
@@ -1,9 +1,6 @@
 package com.mygdx.game.States;
 
-
-
-
-
+import java.util.ArrayList;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
@@ -17,12 +14,15 @@ import com.mygdx.game.RogueMonster;
 import com.mygdx.game.Characters.Character;
 import com.mygdx.game.Characters.Player;
 import com.mygdx.game.Factory.PokemonFactory;
+import com.mygdx.game.Maps.Island;
+import com.mygdx.game.Maps.Tile;
 import com.mygdx.game.Utils.CharacterState;
 
 public class GameState implements Screen {
     // game attributes
     final RogueMonster game;
     Player player;
+    Island island;
 
     Viewport viewport;
     OrthographicCamera camera;
@@ -30,7 +30,7 @@ public class GameState implements Screen {
     PokemonFactory pkmFactory;
     Array<Character> pokemon;
 
-    void spawnEnemy(){
+    void spawnEnemy() {
         pokemon.add(pkmFactory.getPokemon("Mudkip", 0, 0));
         pokemon.add(pkmFactory.getPokemon("Charmander", 0, 64));
         pokemon.add(pkmFactory.getPokemon("Mew", 0, 128));
@@ -44,62 +44,66 @@ public class GameState implements Screen {
         this.game = game;
         player = new Player(500, 500);
 
+        island = new Island();
+
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 1000, 1000);
         viewport = new FitViewport(1000, 1000, camera);
 
         pkmFactory = new PokemonFactory();
-        pokemon = new Array<Character>(); 
+        pokemon = new Array<Character>();
         spawnEnemy();
     }
 
-    
-
     @Override
     public void show() {
-        
+
     }
 
     @Override
     public void render(float delta) {
-       ScreenUtils.clear(0.2f,0,0.2f,1);
-       enlapsedTime += delta;
-       
+        ScreenUtils.clear(0.2f, 0, 0.2f, 1);
+        enlapsedTime += delta;
 
         camera.update();
 
         game.batch.setProjectionMatrix(camera.combined);
 
-       game.batch.begin();
-       //player.draw(game.batch);
-       game.batch.draw(player.getAnimation().getKeyFrame(enlapsedTime, true), player.getX(), player.getY());
-       for(Character iter: pokemon){
+        game.batch.begin();
+        // map draw
+        for (ArrayList<Tile> row : island.chunk.tiles) {
+            for (Tile tile : row) {
+                game.batch.draw(tile.texture, tile.pos.x, tile.pos.y, tile.size, tile.size);
+                if (tile.secondary_texture != null)
+                    game.batch.draw(tile.secondary_texture, tile.pos.x, tile.pos.y, tile.size, tile.size);
+            }
+        }
+        // player.draw(game.batch);
+        game.batch.draw(player.getAnimation().getKeyFrame(enlapsedTime, true), player.getX(), player.getY());
+        for (Character iter : pokemon) {
             game.batch.draw(iter.getAnimation().getKeyFrame(enlapsedTime, true), iter.getX(), iter.getY());
-       }
-       game.batch.end();
+        }
+        game.batch.end();
 
-
-       //player movement system
-       if(Gdx.input.isKeyPressed(Input.Keys.S)){//down
+        // player movement system
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) {// down
             player.movement(0, -1, CharacterState.SOUTH);
-       }else if(Gdx.input.isKeyPressed(Input.Keys.A)){//left
-            player.movement(-1,0,CharacterState.WEST);
-       }else if(Gdx.input.isKeyPressed(Input.Keys.D)){//right
-            player.movement(1,0,CharacterState.EAST);
-       }else if(Gdx.input.isKeyPressed(Input.Keys.W)){//up
-            player.movement(0,1,CharacterState.NORTH);
-       }else{
-            player.movement(0,0, CharacterState.STANDING);
+        } else if (Gdx.input.isKeyPressed(Input.Keys.A)) {// left
+            player.movement(-1, 0, CharacterState.WEST);
+        } else if (Gdx.input.isKeyPressed(Input.Keys.D)) {// right
+            player.movement(1, 0, CharacterState.EAST);
+        } else if (Gdx.input.isKeyPressed(Input.Keys.W)) {// up
+            player.movement(0, 1, CharacterState.NORTH);
+        } else {
+            player.movement(0, 0, CharacterState.STANDING);
         }
 
-        //update pokemon movement
+        // update pokemon movement
         for (Character iter : pokemon) {
             iter.update();
 
-            
         }
-       
-        
+
     }
 
     @Override
