8918009b9dc56693e44341999e70d036b5b6f804
dvrkoo <Nico.marini@hotmail.it>
2022-10-20 12:13:03 +0200

    fixes

--- core/src/com/mygdx/game/Command/UpCommand.java
+++ core/src/com/mygdx/game/Command/UpCommand.java
@@ -3,17 +3,17 @@ package com.mygdx.game.Command;
 import com.mygdx.game.Characters.Player;
 import com.mygdx.game.Utils.Enums.CharacterState;
 
-public class UpCommand implements Command{
+public class UpCommand implements Command {
     Player player;
 
-    public UpCommand(Player player){
+    public UpCommand(Player player) {
         this.player = player;
     }
 
     @Override
     public void execute() {
         player.movement(0, 1, CharacterState.NORTH);
-        
+
     }
-    
+
 }
