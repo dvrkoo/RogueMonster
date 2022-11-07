2f021f5fadad1ecbf045715a2e521776c1ccbabf
dvrkoo <Nico.marini@hotmail.it>
2022-11-07 11:31:49 +0100

    added Junit

---
+++ desktop/build/resources/main/Maps/normal1.tsx
@@ -0,0 +1,74 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="1" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <image source="normal1.png" trans="000000" width="512" height="512"/>
+ <tile id="32">
+  <properties>
+   <property name="Grass" value="true"/>
+  </properties>
+ </tile>
+ <tile id="837">
+  <properties>
+   <property name="Reflection" value="true"/>
+  </properties>
+ </tile>
+ <tile id="838">
+  <properties>
+   <property name="Reflection" value="true"/>
+  </properties>
+ </tile>
+ <tile id="839">
+  <properties>
+   <property name="Reflection" value="true"/>
+  </properties>
+ </tile>
+ <tile id="869">
+  <properties>
+   <property name="Reflection" value="true"/>
+  </properties>
+ </tile>
+ <tile id="870">
+  <properties>
+   <property name="Reflection" value="true"/>
+  </properties>
+ </tile>
+ <tile id="871">
+  <properties>
+   <property name="Reflection" value="true"/>
+  </properties>
+ </tile>
+ <tile id="901">
+  <properties>
+   <property name="Reflection" value="true"/>
+  </properties>
+ </tile>
+ <tile id="902">
+  <properties>
+   <property name="Reflection" value="true"/>
+  </properties>
+ </tile>
+ <tile id="903">
+  <properties>
+   <property name="Reflection" value="true"/>
+  </properties>
+ </tile>
+ <tile id="934">
+  <properties>
+   <property name="Reflection" value="true"/>
+  </properties>
+ </tile>
+ <tile id="935">
+  <properties>
+   <property name="Reflection" value="true"/>
+  </properties>
+ </tile>
+ <tile id="966">
+  <properties>
+   <property name="Reflection" value="true"/>
+  </properties>
+ </tile>
+ <tile id="967">
+  <properties>
+   <property name="Reflection" value="true"/>
+  </properties>
+ </tile>
+</tileset>
